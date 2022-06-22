58564df5059bd1616c9912dd19d05b947aa7edfb
Omar Nava <omar.mac.97@gmail.com>
2022-06-22 18:19:24 -0500

    Initial Commit

---
+++ TabViewProject2/TabViewProject2App.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewProject2App.swift
+//  TabViewProject2
+//
+//  Created by Ricardo Omar Martinez Nava on 22/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TabViewProject2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
